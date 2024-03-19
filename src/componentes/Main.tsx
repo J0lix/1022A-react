48d2516a5273c42bf0273fe84ffe2da1b2a8dfa3
J0lix <julia.bem@estudante.ifms.edu.br>
2024-03-19 10:14:30 -0400

    Box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,9 +1,18 @@
+import "./Main.css"
 function Main(){
     return(
-        <>
-        <main> Main </main>
-        <h1> Meu h1</h1>
-        </>
+        <main> 
+            <div className = "estudante-box">
+                <img className="estudante-foto" src ="https://github.com/J0lix.png" alt = ""/>
+                  <div className ="informacoes">
+                <h3 className="estudante_nome"> Júlia Strey Bem</h3>
+                <p className ="estudante_descrição">Estudante do IFMS, está 5° periodo de informática para a internet</p>
+                <a className="link-git" href ="https://github.com/J0lix">Git Hub </a>
+                 </div>
+            </div>
+
+        </main>
+        
     )
 }
 export default Main;
